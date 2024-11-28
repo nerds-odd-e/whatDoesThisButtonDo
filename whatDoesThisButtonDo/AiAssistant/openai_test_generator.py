2d8f24db9caa69adb8f57743e647f964e9ac1088
Terry Yin <terry.yinzhe@gmail.com>
2024-11-28 20:53:38 +0800

    actually running

--- whatDoesThisButtonDo/AiAssistant/openai_test_generator.py
+++ whatDoesThisButtonDo/AiAssistant/openai_test_generator.py
@@ -1,15 +1,10 @@
 from .openai_client import OpenAIClient
-from .generate_test_cases_command import GenerateTestCasesCommand
 from .get_next_action_command import GetNextActionCommand
 
 class OpenAITestGenerator:
     def __init__(self, test_oracles):
         self.openai_client = OpenAIClient()
         self.test_oracles = test_oracles
-
-    def generate_test_cases(self):
-        command = GenerateTestCasesCommand(self.openai_client, self.test_oracles)
-        return command.execute()
 
     def get_next_action(self, possible_actions):
         """
--- whatDoesThisButtonDo/__init__.py
+++ whatDoesThisButtonDo/__init__.py
@@ -4,11 +4,9 @@ whatDoesThisButtonDo package
 from .AiAssistant.openai_test_generator import OpenAITestGenerator
 from .AiAssistant.openai_client import OpenAIClient
 from .AiAssistant.generate_test_plan_command import GenerateTestPlanCommand
-from .AiAssistant.generate_test_cases_command import GenerateTestCasesCommand
 
 __all__ = [
     'OpenAIClient',
-    'GenerateTestCasesCommand',
     'GenerateTestPlanCommand',
     'OpenAITestGenerator'
 ]
