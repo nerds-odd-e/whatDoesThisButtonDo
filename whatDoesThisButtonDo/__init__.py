c5e47726cd9dcc12a6cac2b524f4fa50fd275f45
Terry Yin <terry.yinzhe@gmail.com>
2024-11-28 20:56:00 +0800

    remove dead code

--- whatDoesThisButtonDo/__init__.py
+++ whatDoesThisButtonDo/__init__.py
@@ -3,11 +3,9 @@ whatDoesThisButtonDo package
 """
 from .AiAssistant.openai_test_generator import OpenAITestGenerator
 from .AiAssistant.openai_client import OpenAIClient
-from .AiAssistant.generate_test_plan_command import GenerateTestPlanCommand
 
 __all__ = [
     'OpenAIClient',
-    'GenerateTestPlanCommand',
     'OpenAITestGenerator'
 ]
 
