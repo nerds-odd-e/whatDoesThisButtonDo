a143658face49e7ccba0ccc238a817a18a3dfc2d
Terry Yin <terry.yinzhe@gmail.com>
2024-11-28 16:57:46 +0800

    pass ai to explore

--- whatDoesThisButtonDo/AiAssistant/openai_test_generator.py
+++ whatDoesThisButtonDo/AiAssistant/openai_test_generator.py
@@ -2,9 +2,10 @@ from .openai_client import OpenAIClient
 from .generate_test_cases_command import GenerateTestCasesCommand
 
 class OpenAITestGenerator:
-    def __init__(self):
+    def __init__(self, test_oracles):
         self.openai_client = OpenAIClient()
+        self.test_oracles = test_oracles
 
-    def generate_test_cases(self, test_oracles):
-        command = GenerateTestCasesCommand(self.openai_client, test_oracles)
+    def generate_test_cases(self):
+        command = GenerateTestCasesCommand(self.openai_client, self.test_oracles)
         return command.execute() 
